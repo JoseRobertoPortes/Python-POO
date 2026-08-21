61df5b0bdd7ce7488cff51473415661723905ee1
JoseRobertoPortes <portes.git@gmail.com>
2026-08-20 22:44:41 -0300

    atualiza `desafio034` e `.gitignore`

--- desafios/desafio034/classes/classes.py
+++ desafios/desafio034/classes/classes.py
@@ -3,56 +3,50 @@ from uteis.dinheiro import formatar_moeda
 
 
 class Funcionario(ABC):
-    def __init__(self, nome, salario:float):
+    def __init__(self, nome: str, salario: float):
         self.nome = nome
         self._salario = salario
 
-    @abstractmethod
-    def  calcularbonus(self):
-        return
-
-class Gerente(Funcionario):
-    def __init__(self, nome, salario):
-        super().__init__(nome, salario)
-
     @property
     def salario(self):
-        fsalario = formatar_moeda(self._salario)
-        return fsalario
+        return self._salario
 
     @salario.setter
-    def salario(self, novo_salario):
+    def salario(self, novo_salario: float):
         if novo_salario > self._salario:
             self._salario = novo_salario
-            print('Salario atualizado com sucesso!')
-            return True
-        else: raise ValueError('Você não pode abaixar o salario de um funcionario.')
+            print('Salário atualizado com sucesso!')
+        elif novo_salario == self._salario:
+            print('O salário informado é igual ao atual.')
+        else:
+            raise ValueError(
+                'Você não pode abaixar o salário de um funcionário.')
 
+    @property
+    def salario_formatado(self):
+        return formatar_moeda(self._salario)
+
+    @abstractmethod
     def calcularbonus(self):
-        percentual = 15/100
-        bonus = formatar_moeda(self._salario * percentual)
-        return bonus
+        pass
+
+    def __str__(self):
+        return f"{self.__class__.__name__} | Nome: {self.nome} | Salário: {self.salario_formatado}"
+
+
+class Gerente(Funcionario):
+    def calcularbonus(self):
+        percentual = 15 / 100
+        return self._salario * percentual
+
 
 class Dev(Funcionario):
-    def __init__(self, nome, salario):
-        super().__init__(nome, salario)
-
     def calcularbonus(self):
-        percentual = 10/100
-        bonus = formatar_moeda(self._salario * percentual)
-        return bonus
+        percentual = 10 / 100
+        return self._salario * percentual
 
 
 class Designer(Funcionario):
-    def __init__(self, nome, salario):
-        super().__init__(nome, salario)
-
     def calcularbonus(self):
-        percentual = 8/100
-        bonus = formatar_moeda(self._salario * percentual)
-        return bonus
-
-
-a = Gerente('Jose', 1000)
-
-print(a)
+        percentual = 8 / 100
+        return self._salario * percentual
